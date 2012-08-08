b310a13960d3959a76e6fcaf82343fea461b244e
Ryan J. Geyer <me@ryangeyer.com>
2012-08-08 16:49:33 -0700

    Added a repos effected counter

--- run_git_cmd_on_all.sh
+++ run_git_cmd_on_all.sh
@@ -3,6 +3,7 @@
 SEARCHDIR=${PWD}
 GITCMD="status"
 TAKEMEHOME=${PWD}
+REPOSFOUND=0
 
 while getopts ":c:d:" optname; do
   case "$optname" in
@@ -22,11 +23,14 @@ echo "Recusively running (git $GITCMD) on $SEARCHDIR"
 
 for i in `find $SEARCHDIR -type d`; do
 	if [ -d "$i/.git" ]; then
+		let "REPOSFOUND = $REPOSFOUND + 1"
 		echo "Why lookie there, $i is a git repository, executing your command"
 		cd $i
 		git $GITCMD
 	fi
 done
+
+echo "Found $REPOSFOUND git repositories and ran your command on all of them"
 
 # We changed directories a ton, let's get back to where we started
 cd $TAKEMEHOME
